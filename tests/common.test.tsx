598440fb651d9e2e9f9afca2996a3fa75a6ac3eb
samvictor <sjinniss1@hotmail.com>
2024-02-09 14:24:00 -0500

    Adding render test

--- tests/common.test.tsx
+++ tests/common.test.tsx
@@ -4,6 +4,7 @@ import { render } from '@testing-library/react'
 import 'jest-canvas-mock'
 
 import { Canvas, Player, Game, Level, Enemy } from '../src'
+import { GameState, LevelState } from '../src/components/library/interfaces'
 
 global.ResizeObserver = jest.fn().mockImplementation(() => ({
   observe: jest.fn(),
@@ -12,10 +13,25 @@ global.ResizeObserver = jest.fn().mockImplementation(() => ({
 }))
 
 describe('Common render', () => {
-  it('renders without crashing', () => {
+  it('renders start screen', () => {
     render(
       <Game>
         <Level>
+          <Canvas>
+            <Enemy color={'red'} objectId='redbox' />
+            <Enemy color={'green'} position={[1, 1, 0]} objectId='greenBox' />
+            <Enemy color={'pink'} position={[-1, 1, 0]} objectId='pinkBox' size={[1.5, 0.5, 1.5]} />
+            <Player />
+          </Canvas>
+        </Level>
+      </Game>,
+    )
+  })
+
+  it('renders normal play', () => {
+    render(
+      <Game settings={{ gameState: GameState.NormalPlay }}>
+        <Level settings={{ levelState: LevelState.NormalPlay }}>
           <Canvas>
             <Enemy color={'red'} objectId='redbox' />
             <Enemy color={'green'} position={[1, 1, 0]} objectId='greenBox' />
