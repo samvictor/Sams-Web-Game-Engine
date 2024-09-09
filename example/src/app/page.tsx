f9cc57f16455eb7fed8ce5b777a3ab323cdefa88
samvictor <sjinniss1@hotmail.com>
2024-09-09 16:20:32 -0400

    Including example

---
+++ example/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+import styles from './page.module.css';
+
+// import { Test, ExampleGame } from 'sams-web-game-engine';
+import SimpleExample from './simpleExample';
+
+// import LevelsExample from './levelsExample';
+
+export default function Home() {
+  return <SimpleExample />;
+}
