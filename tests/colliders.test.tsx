9ff14d745ded05e7666ea99d9ab1e8de05ae8121
samvictor <sjinniss1@hotmail.com>
2024-10-07 21:14:31 -0400

    Adding tests back in

--- tests/colliders.test.tsx
+++ tests/colliders.test.tsx
@@ -3,99 +3,93 @@ import { render } from '@testing-library/react';
 
 import 'jest-canvas-mock';
 
-describe('placeholder', () => {
-  it('holds a place', () => {
-    expect(1).toBe(1);
+import { collisionCheck } from '../src/components/library/helpfulFunctions';
+import { ColliderShape } from '../src/components/library/interfaces';
+
+describe('Collisions', () => {
+  describe('Sphere to sphere', () => {
+    test('miss evaluates to false', () => {
+      const object1 = {
+        position: [0, 2, 0],
+        collider: {
+          shape: ColliderShape.Sphere,
+          sphereRadius: 1,
+        },
+      };
+
+      const object2 = {
+        position: [2, 0, 0],
+        collider: {
+          shape: ColliderShape.Sphere,
+          sphereRadius: 1,
+        },
+      };
+
+      expect(collisionCheck(object1, object2)).toBe(false);
+      expect(collisionCheck(object2, object1)).toBe(false);
+    });
+
+    test('hit evaluates to true', () => {
+      const object1 = {
+        position: [0, 1, 0],
+        collider: {
+          shape: ColliderShape.Sphere,
+          sphereRadius: 1,
+        },
+      };
+
+      const object2 = {
+        position: [1, 0, 0],
+        collider: {
+          shape: ColliderShape.Sphere,
+          sphereRadius: 1,
+        },
+      };
+
+      expect(collisionCheck(object1, object2)).toBe(true);
+      expect(collisionCheck(object2, object1)).toBe(true);
+    });
+  });
+
+  describe('Point to box', () => {
+    test('miss evaluates to false', () => {
+      const object1 = {
+        position: [0, 2, 0],
+        collider: {
+          shape: ColliderShape.Point,
+        },
+      };
+
+      const object2 = {
+        position: [2, 0, 0],
+        collider: {
+          shape: ColliderShape.Box,
+          boxSize: [1, 1, 1],
+        },
+      };
+
+      expect(collisionCheck(object1, object2)).toBe(false);
+      expect(collisionCheck(object2, object1)).toBe(false);
+    });
+
+    test('hit evaluates to true', () => {
+      const object1 = {
+        position: [1, 0.5, 0],
+        collider: {
+          shape: ColliderShape.Point,
+        },
+      };
+
+      const object2 = {
+        position: [1, 0, 0],
+        collider: {
+          shape: ColliderShape.Box,
+          boxSize: [1, 1, 1],
+        },
+      };
+
+      expect(collisionCheck(object1, object2)).toBe(true);
+      expect(collisionCheck(object2, object1)).toBe(true);
+    });
   });
 });
-
-// import { collisionCheck } from '../src';
-// import { ColliderShape } from '../src/components/library/interfaces';
-
-// describe('Collisions', () => {
-//   describe('Sphere to sphere', () => {
-//     test('miss evaluates to false', () => {
-//       const object1 = {
-//         position: [0, 2, 0],
-//         collider: {
-//           shape: ColliderShape.Sphere,
-//           sphereRadius: 1,
-//         },
-//       };
-
-//       const object2 = {
-//         position: [2, 0, 0],
-//         collider: {
-//           shape: ColliderShape.Sphere,
-//           sphereRadius: 1,
-//         },
-//       };
-
-//       expect(collisionCheck(object1, object2)).toBe(false);
-//       expect(collisionCheck(object2, object1)).toBe(false);
-//     });
-
-//     test('hit evaluates to true', () => {
-//       const object1 = {
-//         position: [0, 1, 0],
-//         collider: {
-//           shape: ColliderShape.Sphere,
-//           sphereRadius: 1,
-//         },
-//       };
-
-//       const object2 = {
-//         position: [1, 0, 0],
-//         collider: {
-//           shape: ColliderShape.Sphere,
-//           sphereRadius: 1,
-//         },
-//       };
-
-//       expect(collisionCheck(object1, object2)).toBe(true);
-//       expect(collisionCheck(object2, object1)).toBe(true);
-//     });
-//   });
-
-//   describe('Point to box', () => {
-//     test('miss evaluates to false', () => {
-//       const object1 = {
-//         position: [0, 2, 0],
-//         collider: {
-//           shape: ColliderShape.Point,
-//         },
-//       };
-
-//       const object2 = {
-//         position: [2, 0, 0],
-//         collider: {
-//           shape: ColliderShape.Box,
-//           boxSize: [1, 1, 1],
-//         },
-//       };
-
-//       expect(collisionCheck(object1, object2)).toBe(false);
-//       expect(collisionCheck(object2, object1)).toBe(false);
-//     });
-
-//     test('hit evaluates to true', () => {
-//       const object1 = {
-//         position: [1, 0.5, 0],
-//         collider: {
-//           shape: ColliderShape.Point,
-//         },
-//       };
-
-//       const object2 = {
-//         position: [1, 0, 0],
-//         collider: {
-//           shape: ColliderShape.Box,
-//           boxSize: [1, 1, 1],
-//         },
-//       };
-
-//       expect(collisionCheck(object1, object2)).toBe(true);
-//       expect(collisionCheck(object2, object1)).toBe(true);
-//     });
-//   });
-// });
